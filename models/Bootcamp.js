6dc379f5c3ab37c6915543d0be9aa824ee86d9ae
Tauhidul Islam <abdurshahin5@gmail.com>
2021-02-16 05:33:17 +0600

    added error handling

---
+++ models/Bootcamp.js
@@ -0,0 +1,99 @@
+const mongoose = require('mongoose');
+
+const BootCampSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add a name'],
+    unique: true,
+    trim: true,
+    maxlength: [50,'Name can not be more than 50 characters']
+  },
+  slug: String,
+  description: {
+    type: String,
+    required: [true, 'Please add a name'],
+    unique: true,
+    trim: true,
+    maxlength: [500,'description can not be more than 500 characters']
+  },
+  website: {
+    type: String
+  },
+  phone:{
+    type: String,
+    maxlength: [20, 'Phone number can not be longer than 20 characters']
+  },
+  email:{
+    type: String,
+  },
+  address:{
+    type: String,
+    required: [true, 'Please add an address']
+
+  },
+  // location:{
+  //   // GeoJSON point
+  //   type:{
+  //     type: String,
+  //     enum: ['Point'],
+  //     required: true
+  //   },
+  //   coordinates: {
+  //     type: [Number],
+  //     required: true,
+  //     index: '2dsphere'
+  //   },
+  //   formattedAddress: String,
+  //   street: String,
+  //   city: String,
+  //   state: String,
+  //   zipcode: String,
+  //   country: String
+
+  // },
+  careers: {
+    type: [String],
+    required: true,
+    enum: [
+      'Web Development',
+      'Mobile Development',
+      'UI/UX',
+      'Data Science',
+      'Business',
+      'Other',
+    ]
+  },
+  averageRating: {
+    type: Number,
+    min: [1, 'Rating must be at least 1'],
+    max: [10, 'Rating must can not be more than 10']
+  },
+  averageCost: Number,
+  photo:{
+    type: String,
+    default: 'no-photo.jpg'
+  },
+  housing: {
+    type: Boolean,
+    default: false
+  },
+  jobAssistance: {
+    type: Boolean,
+    default: false
+  },
+  jobGuarantee: {
+    type: Boolean,
+    default: false
+  },
+  acceptGi: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+
+});
+
+module.exports = mongoose.model('Bootcamp', BootCampSchema)
---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const dotenv = require('dotenv');
+// const logger = require('./middleware/logger')
+const morgan = require('morgan')
+const connectDB = require('./config/db');
+const colors = require('colors')
+const errorHandler = require('./middleware/error')
+
+// Load env vars
+dotenv.config({path: './config/config.env'});
+
+// Connect to database 
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
+const app = express();
+
+// Body parser
+app.use(express.json())
+
+// app.use(logger);
+
+// Dev loggin middleware
+if(process.env.NODE_ENV === 'development'){
+  app.use(morgan('dev'));
+}
+
+// Mount routers
+app.use('/api/v1/bootcamps',bootcamps);
+
+
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection',(err,promise)=>{
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit process
+  server.close(()=>process.exit(1));
+});
