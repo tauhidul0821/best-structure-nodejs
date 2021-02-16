130676d028fba865813168d9305f1fb07947c226
tauhidbs23 <tauhidul.islam@brainstation23.com>
2021-02-16 16:19:34 +0600

    added duplicate field value entered

--- middleware/error.js
+++ middleware/error.js
@@ -19,6 +19,12 @@ const errorHandler = (err, req, res, next) => {
 
   }
 
+  // Mongoose validation error 
+  if(err.name === 'ValidationError'){
+    const message = Object.values(err.errors).map(val =>val.message);
+    error = new ErrorResponse(message,400);
+  }
+
   res.status(err.statusCode || 500).json({ success: false, error: err.message })
 
 }
