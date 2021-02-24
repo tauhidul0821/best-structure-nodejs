46da96da663d5b02d00dfe44313c59d16061b643
tauhidbs23 <tauhidul.islam@brainstation23.com>
2021-02-24 18:47:20 +0600

    now api have sort and select filter

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -7,15 +7,39 @@ const Bootcamp = require('../models/Bootcamp');
 // @route   GET /api/v1/bootcamps
 // @access  Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-
   let query;
-  let queryStr = JSON.stringify(req.query);
 
-  queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match =>`$${match}`);
+  // Copy req.query
+  const reqQuery = { ...req.query };
 
-  console.log(queryStr);
+  // Fields to exclude
+  const removeFields = ['select', 'sort'];
+
+  // Loop over removeFields and delete them from reqQuery
+  removeFields.forEach(param => delete reqQuery[param])
+
+  // Create query string
+  let queryStr = JSON.stringify(reqQuery);
+
+  // Create operators ($gt, $gte, etc)
+  queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
 
   query = Bootcamp.find(JSON.parse(queryStr));
+
+  // Select fields
+  if (req.query.select) {
+    const fields = req.query.select.split(',').join(' ');
+    query = query.select(fields)
+    console.log('line 34', query);
+  }
+
+  // Sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort('-createdAt');
+  }
 
   const bootcamps = await query
   res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps });
