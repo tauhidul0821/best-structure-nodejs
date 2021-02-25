ccb42221dda37ec992c01c0036ce65330540061b
tauhidbs23 <tauhidul.islam@brainstation23.com>
2021-02-25 15:56:47 +0600

    seeder create for courses

--- seeder.js
+++ seeder.js
@@ -9,6 +9,7 @@ dotenv.config({ path: './config/config.env' });
 
 // Load models
 const Bootcamp = require('./models/Bootcamp');
+const Course = require('./models/Course');
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
@@ -19,10 +20,12 @@ mongoose.connect(process.env.MONGO_URI, {
 
 // Read JSON files
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`,'utf-8'));
+const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`,'utf-8'));
 
 // Import into DB
 const importData = asyncHandler(async () => {
   await Bootcamp.create(bootcamps);
+  await Course.create(courses);
   console.log('Data Imported...'.green.inverse);
   process.exit();
 });
@@ -30,6 +33,7 @@ const importData = asyncHandler(async () => {
 // Delete data
 const deleteData = asyncHandler(async () => {
   await Bootcamp.deleteMany();
+  await Course.deleteMany();
   console.log('Data Destroyed...'.red.inverse);
   process.exit();
 });
