283771a4cf00b107b8b84aff0c1357065c763093
tauhidbs23 <tauhidul.islam@brainstation23.com>
2021-02-26 15:49:16 +0600

    update and delete course

--- controllers/courses.js
+++ controllers/courses.js
@@ -44,10 +44,8 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
   res.status(200).json({ success: true, data: course });
 });
 
-
-
 // @desc    Add course
-// @route   POST /api/v1/bootcamps/:bootcampId/course
+// @route   POST /api/v1/bootcamps/:bootcampId/courses
 // @access  Private
 exports.addCourse = asyncHandler(async (req, res, next) => {
   req.body.bootcamp = req.params.bootcampId;
@@ -57,14 +55,48 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
     select: 'name description'
   });
 
-
   if (!bootcamp) {
     return next(new ErrorResponse(`No bootcamp found with id of ${req.params.bootcampId}`, 404));
   }
 
-
-
   const course = await Course.create(req.body);
   res.status(201).json({ success: true, data: course });
+
+});
+
+
+
+// @desc    Update course
+// @route   PUT /api/v1/courses/:id
+// @access  Private
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+  let course = await Course.findById(req.params.id);
+
+  if (!course) {
+    return next(new ErrorResponse(`No course found with id of ${req.params.id}`, 404));
+  }
+
+  course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({ success: true, data: course });
+
+});
+
+// @desc    Delete course
+// @route   DELETE /api/v1/courses/:id
+// @access  Private
+exports.deleteCourse = asyncHandler(async (req, res, next) => {
+  let course = await Course.findById(req.params.id);
+
+  if (!course) {
+    return next(new ErrorResponse(`No course found with id of ${req.params.id}`, 404));
+  }
+
+  await course.remove();
+
+  res.status(200).json({ success: true, data: {} });
 
 });
--- routes/courses.js
+++ routes/courses.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
-const { getCourses, getCourse, addCourse } = require('../controllers/courses');
+const { getCourses, getCourse, addCourse, updateCourse, deleteCourse } = require('../controllers/courses');
 
 router
   .route('/')
@@ -9,8 +9,8 @@ router
 
 router
   .route('/:id')
-  .get(getCourse);
-//   .put(updateBootcamp)
-//   .delete(deleteBootcamp);
+  .get(getCourse)
+  .put(updateCourse)
+  .delete(deleteCourse);
 
 module.exports = router
