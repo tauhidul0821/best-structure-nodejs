54dae70dd14cf4f801766a7a4ae352ab77113784
tauhidbs23 <tauhidul.islam@brainstation23.com>
2021-02-26 16:18:00 +0600

    added aggregate function

--- models/Course.js
+++ models/Course.js
@@ -38,4 +38,37 @@ const CourseSchema = new mongoose.Schema({
   }
 });
 
-module.exports = mongoose.model('Course',CourseSchema)
+// Static method to get avg of course tuitions
+CourseSchema.static.getAverageCost = async function (bootcampId) {
+  console.log('Calculating avg cost ...'.blue);
+
+  const obj = await this.aggregate([
+    {
+      $match: { bootcamp: bootcampId }
+    },
+    {
+      $group: {
+        _id: '$bootcamp',
+        averageCost: { $avg: '$tuition' }
+      }
+    }
+  ])
+
+  console.log(obj);
+
+}
+
+
+// Call getAverageCost after save
+CourseSchema.post('save', function () {
+  this.constructor.getAverageCost(this.bootcamp);
+});
+
+// Call getAverageCost after remove
+CourseSchema.pre('remove', function () {
+  this.constructor.getAverageCost(this.bootcamp);
+});
+
+
+
+module.exports = mongoose.model('Course', CourseSchema)
